8c79e48e742dab4178a8e0e5523f0f240c9c062b
Siddharth Bhawalkar <siddharthdb@gmail.com>
2020-09-23 12:09:52 -0400

    Added unit test using Jest

---
+++ test/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react';
+
+import App from '../src/Components/App';
+import { MemoryRouter } from 'react-router-dom';
+
+test('renders my blabber mouth text', () => {
+  const { getByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  const linkElement = getByText(/My Blabber Mouth/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ test/ArticlePage.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react';
+
+import { ArticlePage } from '../src/Pages/ArticlePage';
+
+test('renders Test Name in article page', () => {
+  const { getByText } = render(<ArticlePage required={true}
+    match={{params: {name: "Test Name"}}} />);
+
+  const linkElement = getByText(/Article - Test Name/i);
+  expect(linkElement).toBeInTheDocument();
+});
